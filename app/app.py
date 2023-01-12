d1c5ff8497eb07b67a96e780263541e53130c3de
vikilog <svikasswami03@gmail.com>
2023-01-12 20:12:46 +0530

    bug fixes

--- app/app.py
+++ app/app.py
@@ -9,20 +9,23 @@ from werkzeug.exceptions import Forbidden
 from pylti1p3.contrib.flask import FlaskOIDCLogin, FlaskMessageLaunch, FlaskRequest, FlaskCacheDataStorage
 from pylti1p3.tool_config import ToolConfJsonFile
 from pylti1p3.registration import Registration
+import datetime
 
 
 PAGE_TITLE = 'Canvas LTI Framework with API Integration'
 API_BASE = 'https://biztechcollege.instructure.com'
 CLIENT_ID_API = 187330000000000107
 CLIENT_SECRET_API = 'qRNvqmFP0my9gs2cyiDemPaTHdIQxfws4hemr4tqOk4REc06lkuvHmzV4xT2trjr'#18733~NH2yqQUAicdAUdUM2UsVECjW6RorTc0StQi82uL8YdSiCjkg9RTWcphF9UsIwEVM
-REDIRECT_URI_API = 'https://9001-sumitb007-ltitest-tqzr9hg17hc.ws-us81.gitpod.io/oauth/'
+REDIRECT_URI_API = 'https://9156-2400-80c0-2001-c30-a9ba-71ce-11cc-1a84.in.ngrok.io/oauth/'
 OAUTH_URL = '{}/login/oauth2/auth?client_id={}&response_type=code&redirect_uri={}&state=BIZTECH'.format(
     API_BASE,
     CLIENT_ID_API,
     REDIRECT_URI_API
 )
 
-#log_creds={}
+log_creds={}
+
+datetime.datetime.now()
 
 class ReverseProxied:
     def __init__(self, app):
@@ -79,16 +82,28 @@ def get_jwk_from_public_key(key_name):
     return jwk
 
 def get_api_token_authorization(code):
-    api = '{}/login/oauth2/token'.format(API_BASE)
-    params = {
+    try:
+        access_token = cache.get('access_token')
+        refresh_token = cache.get('refresh_token')
+        if access_token and refresh_token:
+            return {
+                'access_token': access_token,
+                'refresh_token': refresh_token
+            }
+        api = '{}/login/oauth2/token'.format(API_BASE)
+        params = {
         'grant_type': 'authorization_code',
         'client_id': CLIENT_ID_API,
         'client_secret': CLIENT_SECRET_API,
         'redirect_uri': REDIRECT_URI_API,
         'code': code
-    }
-    r = requests.post(api, params=params).json()
-    return r
+        }
+        r = requests.post(api, params=params).json()
+        cache.set('access_token', r['access_token'])
+        cache.set('refresh_token', r['refresh_token'])
+        return r
+    except Exception as e:
+        print(e)
 
 def get_site_student_enrollment_count(canvas_site_id, access_token):
     api = '{}/api/v1/courses/{}'.format(API_BASE, canvas_site_id)
@@ -139,12 +154,17 @@ def get_site_assess_submission(canvas_site_id, access_token,assignment_id,user_i
     r = requests.put(api, params=params, headers=headers).json()
     return r
 
-'''def assess(x):
+def assess(x):
     if(x=="pass"):
         return 10
     else:
-        return 0'''
-  
+        return 0
+
+
+@app.route('/index/', methods=['GET'])
+def index():
+    return '<form action="/launch/" method="post"><input type="submit" value="Login"></form>'
+
 
 @app.route('/login/', methods=['GET', 'POST'])
 def login():
@@ -200,7 +220,7 @@ def dance():
 
 @app.route('/oauth/', methods=['GET', 'POST'])
 def oauth():
-
+    print(request.args)
     tool_conf = ToolConfJsonFile(get_lti_config_path())
     flask_request = FlaskRequest()
     launch_data_storage = get_launch_data_storage()
@@ -224,7 +244,7 @@ def oauth():
         student_submissions = get_site_student_submissions(course_id, r['access_token'], 2851)
         submission_assessment = get_site_assess_submission(course_id, r['access_token'], 2851, 709, 30)
     
-    '''if (request.method == "POST"):
+    if (request.method == "POST"):
         Sub1 = request.form.get("Sub1")
         Sub2 = request.form.get("Sub2")
         Sub3 = request.form.get("Sub3")
@@ -234,14 +254,15 @@ def oauth():
 
         marks=0
 
+
         marks += assess(Sub1)
         marks += assess(Sub2)
         marks += assess(Sub3)
         marks += assess(Sub4)
-
-        submission_assessment = get_site_assess_submission(log_creds['course_id'], log_creds['access_token'], assign_id, student_id, marks)
+        print(marks)
+        submission_assessment = get_site_assess_submission(course_id, r['access_token'], assign_id, student_id, marks)
         
-        return redirect(request.referrer)'''
+        return redirect(request.referrer)
     
     params = {
         'code': code,
@@ -251,7 +272,7 @@ def oauth():
         #'launch_id': message_launch.get_launch_id(),
         'user_name': message_launch_data.get('name', ''),
         'user_email': message_launch_data.get('email', ''),
-        'user_id': r['user']['id'],
+        'user_id': 709,
         'user_sis_id': message_launch_data.get('https://purl.imsglobal.org/spec/lti/claim/lis', {}).get('person_sourcedid', None),
         'course_title': message_launch_data.get('https://purl.imsglobal.org/spec/lti/claim/context',{}).get('title', None),
         'course_id': course_id,
@@ -265,7 +286,7 @@ def oauth():
         
     }
     
-    ##log_creds.update(params)
+    log_creds.update(params)
 
     return render_template('index.html', **params)
 
